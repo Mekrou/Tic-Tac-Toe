d7d20b02db611f29f742fa06b409e3c0193806c0
Mekrou <mek.rmills@gmail.com>
2021-01-31 13:14:48 -0500

    Added clean playerInput Verification

--- PlayerHandler.cs
+++ PlayerHandler.cs
@@ -4,22 +4,18 @@ namespace Tic_Tac_Toe
 {
     enum PlayerState
     {
-        X,
-        O
-    }
-
-    enum PlayerKey
-    {
-        Q = 1, W = 2, E = 3,
-        A = 4, S = 5, D = 6,
-        Z = 7, X = 8, C = 9
+        X = 1,
+        O = 2
     }
 
     class PlayerHandler
     {   
         private PlayerState currentPlayerState;
-        private PlayerKey currentPlayerKey;
-
+        private ConsoleKey[] validConsoleKeys = new ConsoleKey[]   {ConsoleKey.Q, ConsoleKey.W, ConsoleKey.E, 
+                                                                    ConsoleKey.A, ConsoleKey.S, ConsoleKey.D, 
+                                                                    ConsoleKey.Z, ConsoleKey.C, ConsoleKey.X, 
+                                                                    ConsoleKey.O, ConsoleKey.Escape, ConsoleKey.Enter};
+        private bool gameOver = false;
         public  PlayerState CurrentPlayerState
         {
             get
@@ -32,21 +28,58 @@ namespace Tic_Tac_Toe
             }
         }
 
-        public PlayerKey CurrentPlayerKey
-        {
-            get
-            {
-                return currentPlayerKey;
-            }
-            set
-            {
-                currentPlayerKey = value;
-            }
-        }
 
         public void PlayerMove()
         {
-            Console.WriteLine("Entered PlayerMove()");
+            Board board = new Board();
+            Console.WriteLine(currentPlayerState);
+            do
+            {
+                if (currentPlayerState.Equals(PlayerState.X))
+                {
+                    // Get player's desired tile
+                    Console.Clear();
+                    board.PrintControlBoard();
+                    Console.WriteLine("Where would you like to place your 'X'?");
+                    ConsoleKey playerInput = GetPlayerInput();
+                    
+                    
+                    gameOver = true;
+                    break;
+                    // Place 'X' There
+                    // Move turn to 'O'
+                    // this.currentPlayerState = PlayerState.O;
+                } else if (currentPlayerState.Equals(PlayerState.O))
+                {
+                    Console.WriteLine("Where would you like to place your 'Y'?");
+                    this.currentPlayerState = PlayerState.X;
+                }
+            } while (!gameOver);
+        }
+
+        public ConsoleKey GetPlayerInput()
+        {
+            ConsoleKey playerInput;
+            bool isValidInput = false;
+
+            do
+            {
+                playerInput = Console.ReadKey(true).Key;
+                foreach (ConsoleKey key in validConsoleKeys)
+                {
+                    if (playerInput.Equals(key))
+                    {
+                        isValidInput = true;
+                        break;
+                    } 
+                }
+                if (isValidInput == false)
+                {
+                    Console.WriteLine("Invalid Input! Did you forget the controls?");
+                }
+            } while (!isValidInput);
+
+            return playerInput;
         }
     }
 }
